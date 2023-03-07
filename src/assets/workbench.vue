eb093dda35cdbba6424fbed6ddee7dae9165dc7c
kobewiththefadeaway <118722212+kobewiththefadeaway@users.noreply.github.com>
2023-03-07 14:42:45 +0100

    cockclock

--- src/assets/workbench.vue
+++ src/assets/workbench.vue
@@ -5,13 +5,11 @@
       <div class="col-sm-12 bg-info m-3"></div>
     </div>
   </div>
+  
 
 'WelcomeToOpportunity'
 
-<template>
- 
-  
-</template>
+
 
 <script>
 /* WelcomeOpportunity.vue ln 28 -32 das
